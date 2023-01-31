94ff7f46e787c9d5fc3fc6a1e3aaf5b31ed387f1
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-31 13:53:45 -0500

    Update RocketList and main to work with redux.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import store from './redux/configureStore';
+import { gettingRockets } from './redux/rockets/rocketsSlice';
 import './index.css';
 
+store.dispatch(gettingRockets());
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
