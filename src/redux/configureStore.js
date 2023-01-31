d361345c9dded4baa613ff1c318365be20c7c922
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-01-31 05:37:40 -0500

    Fix linter errors.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import rocketsReducer from './rockets/rockets';
+import rocketsReducer from './rockets/rocketsSlice';
 
 const store = configureStore({
   reducer: {
