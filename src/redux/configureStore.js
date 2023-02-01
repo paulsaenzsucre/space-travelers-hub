8779bd68b3f64916e62f4128991a6588445b8c49
GutemaG <bir13gu17@gmail.com>
2023-02-01 23:29:18 +0300

    Display missions and toggle between states

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import missionReducer from './missions/missions';
 import rocketsReducer from './rockets/rocketsSlice';
 
+const rootReducer = combineReducers({
+  rockets: rocketsReducer,
+  missions: missionReducer,
+});
+
 const store = configureStore({
-  reducer: {
-    rockets: rocketsReducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
